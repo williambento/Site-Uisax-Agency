fa1bccbaa5a848b9f64b7543a8b42c07dd950a20
williambento <115882853+williambento@users.noreply.github.com>
2023-10-06 09:22:10 -0300

    06/10
    atualização portfolio

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -11,19 +11,19 @@ export default function Portfolio(){
         <div className={styles.portfolio}>
             <h1>PORTFOLIO</h1>
             <div className={styles.projeto}>
-                <a href="" className={styles.btimg}>
+                <a href="https://drive.google.com/file/d/1P4xolr0L5Uz_Fd1yXOAKYB-fGRyw4uWk/view?usp=drive_link" className={styles.btimg}>
                     <Image src={company} alt="FJ Company" width={200} height={200}/>
                     <button> VER MAIS </button>
                 </a>
-                <a href="" className={styles.btimg}>
+                <a href="https://drive.google.com/file/d/1Rhj_dGoCNbeySk_K6HYLQgib-Nz2DELN/view?usp=drive_link" className={styles.btimg}>
                     <Image src={system} alt="Ocka System" width={200} height={200}/>
                     <button> VER MAIS </button>
                 </a>
-                <a href="" className={styles.btimg}>
+                <a href="https://drive.google.com/file/d/1UV0ZeKrs8REzeUYxLZ6QMpMkreMhMnIW/view?usp=drive_link" className={styles.btimg}>
                     <Image src={rdo} alt="RDO Consultoria" width={200} height={200}/>
                     <button> VER MAIS </button>
                 </a>
-                <a href="" className={styles.btimg}>
+                <a href="https://drive.google.com/file/d/1QxrhouZm9yEVI06dYbqqqnC4MXl5wyPa/view?usp=drive_link" className={styles.btimg}>
                     <Image src={jufontans} alt="Ju Fontans" width={200} height={200}/>
                     <button> VER MAIS </button>
                 </a>
